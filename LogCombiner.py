69779548af694203637a7d5274e159f7cfbe10e7
m1nsk <punch.korolev@yandex.ru>
2017-11-16 20:27:03 +0300

    moved LogCombiner class into module

---
+++ LogCombiner.py
@@ -0,0 +1,57 @@
+import datetime
+import re
+import time
+import heapq
+from collections import namedtuple
+import os
+
+log_data = namedtuple('log_data', ['data', 'string', 'file'])
+
+
+class LogDateException(Exception):
+    pass
+
+
+class LogCombiner:
+    @staticmethod
+    def parse_date(string, reg_pattern):
+        date_match = re.match(reg_pattern, string)
+        if date_match is None:
+            raise LogDateException
+        date = date_match.group(1)
+        return time.strptime(date, '%d-%m-%Y %H:%M:%S')
+
+    def __init__(self, result_file_name, log_files, date_parse_pattern):
+        self.log_heap = []
+        self.parse_pattern = date_parse_pattern
+        self.log_files = log_files
+        self._init__log_generator()
+
+    def _push_heap_data(self, log_file):
+        push_flag = True
+        while push_flag:
+            log_string = log_file.readline()
+            if log_string:
+                try:
+                    date = LogCombiner.parse_date(log_string, self.parse_pattern)
+                    heapq.heappush(self.log_heap, (date, log_string, log_file))
+                    push_flag = False
+                except LogDateException:
+                    push_flag = True
+            else:
+                self.log_files.remove(log_file)
+                push_flag = False
+
+    def _pop_heap_data(self):
+        return heapq.heappop(self.log_heap)
+
+    def _init__log_generator(self):
+        for log in self.log_files:
+            self._push_heap_data(log)
+
+    def log_generator(self):
+        while len(self.log_files):
+            log_item = log_data(*self._pop_heap_data())
+            yield log_item.string
+            self._push_heap_data(log_item.file)
+        return
