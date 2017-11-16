93c543b35871e9dde48ba7336730378b4f02eeb9
m1nsk <punch.korolev@yandex.ru>
2017-11-16 22:29:29 +0300

    added command line arguments

--- log_join.py
+++ log_join.py
@@ -1,9 +1,14 @@
 import LogCombiner
 import os
+import sys
 
 
 def main():
-    logs = ['log1.txt', 'log2.txt', 'log3.txt']
+    args = sys.argv[1:]
+    if len(args):
+        logs = args
+    else:
+        logs = ['log1.txt', 'log2.txt', 'log3.txt']
     my_dir = os.path.dirname(__file__)
     logs = list(map(lambda log: os.path.join(my_dir, log), logs))
     log_files = []
@@ -14,7 +19,6 @@ def main():
         log_combiner = LogCombiner.LogCombiner('test', log_files, r'\[(.+)\]\s+')
         with open('test_log.txt', 'w', encoding='utf-8') as file:
             for log_string in log_combiner.log_generator():
-                print(log_string)
                 file.write(log_string)
     finally:
         for log in log_files:
