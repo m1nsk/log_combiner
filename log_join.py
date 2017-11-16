9b1924f92958191a753b7cab08446d648464e2b9
m1nsk <punch.korolev@yandex.ru>
2017-11-16 22:30:33 +0300

    added re_pattern

--- log_join.py
+++ log_join.py
@@ -11,12 +11,13 @@ def main():
         logs = ['log1.txt', 'log2.txt', 'log3.txt']
     my_dir = os.path.dirname(__file__)
     logs = list(map(lambda log: os.path.join(my_dir, log), logs))
+    re_pattern = r'\[(.+)\]\s+'
     log_files = []
 
     try:
         for log in logs:
             log_files.append(open(log))
-        log_combiner = LogCombiner.LogCombiner('test', log_files, r'\[(.+)\]\s+')
+        log_combiner = LogCombiner.LogCombiner('test', log_files, re_pattern)
         with open('test_log.txt', 'w', encoding='utf-8') as file:
             for log_string in log_combiner.log_generator():
                 file.write(log_string)
